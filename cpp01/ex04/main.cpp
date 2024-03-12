286397132883ca7f9e863d2d1951e48fa185eea2
Omar <omar.tourabi7@gmail.com>
2024-03-12 15:53:01 +0100

    Update(cpp01): remove unused code

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <fstream>
 #include "Sed.hpp"
+
 int isValidArgs(int argc, char const *argv[])
 {
     if (argc != 4)
@@ -8,24 +9,10 @@ int isValidArgs(int argc, char const *argv[])
         std::cout << "[USAGE]: <filename> <string_to_replace> <new_string>" << std::endl;
         return (0);
     }
-    std::string str_to_replace = argv[2]; 
+    std::string str_to_replace = argv[2];
     if (str_to_replace.empty())
         return (0);
     return (1);
-}
-
-void copyFile(char const *argv) 
-{ 
-    std::string filename = argv;
-    std::string output =  filename + std::string(".replace");
-    std::ifstream file(argv);
-    std::ofstream copy(output.c_str());
-    std::string read;
-    while (std::getline(file, read))
-        copy << read << std::endl;
-
-    file.close();
-    copy.close();
 }
 
 int main(int argc, char const *argv[])
