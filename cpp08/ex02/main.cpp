314a23377938cd4a2334542ba206a2b58df3153b
Tourabi Omar <omar.tourabi7@gmail.com>
2024-07-03 14:44:37 +0200

    UPDATE: cpp08 OK

--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -1,28 +1,39 @@
-#include <iostream>
 #include "MutantStack.h"
+#include <iostream>
 
 
-int main()
-{
-  MutantStack<int> mstack;
-  mstack.push(5);
-  mstack.push(17);
-  std::cout << mstack.top() << std::endl;
-  mstack.pop();
-  std::cout << mstack.size() << std::endl;
-  mstack.push(3);
-  mstack.push(5);
-  mstack.push(737); //[...] mstack.push(0);
-  MutantStack<int>::iterator it = mstack.begin();
-  MutantStack<int>::iterator ite = mstack.end();
-  ++it;
-  --it;
-  while (it != ite)
+int main() {
   {
-    std::cout << *it << std::endl;
+    MutantStack<int> mstack;
+    mstack.push(5);
+    mstack.push(17);
+    std::cout << mstack.top() << std::endl;
+    mstack.pop();
+    std::cout << mstack.size() << std::endl;
+    mstack.push(3);
+    mstack.push(5);
+    mstack.push(737); //[...] mstack.push(0);
+    MutantStack<int>::iterator it = mstack.begin();
+    MutantStack<int>::iterator ite = mstack.end();
     ++it;
+    --it;
+    while (it != ite) {
+      std::cout << *it << std::endl;
+      ++it;
+    }
+    std::stack<int> s(mstack);
   }
-  std::stack<int> s(mstack);
 
+  {
+    std::cout << std::endl;
+    MutantStack<float> st;
+
+    st.push(39.09);
+    st.push(239.09);
+    st.push(339.11);
+    st.push(349.124);
+    std::cout << *st.begin() << std::endl;
+    std::cout << *--st.end() << std::endl;
+  }
   return 0;
 }
